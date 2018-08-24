1eb3ee5574b5808e4f250581b2ebc570e82a28bc
ipresinal <ivan.presinal@hotmail.com>
2018-08-24 15:47:04 -0400

    setup resources

--- WordChat/WordChat/MainWindow.xaml.cs
+++ WordChat/WordChat/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WordChat.ViewModel;
 
 namespace WordChat
 {
@@ -23,6 +24,8 @@ namespace WordChat
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new WindowViewModel(this);
         }
     }
 }
---
+++ WordChat/WordChat/ViewModel/WindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WordChat.ViewModel
+{
+    /// <summary>
+    /// The View Model for the custom flat window
+    /// </summary>
+    public class WindowViewModel : BaseViewModel
+    {
+        #region Private Member
+
+        /// <summary>
+        /// The window this view model controls
+        /// </summary>
+        private Window mWindow;
+
+        #endregion
+
+
+        #region Public Properties
+
+        public string Test { get; set; } = "MyString";
+
+        #endregion
+
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="window"></param>
+        public WindowViewModel(Window window)
+        {
+            mWindow = window;
+        }
+
+        #endregion
+    }
+}
